2b7380b94ddd0b31f1044c754d9d49c2a1e626e9
Justin Boggs <jboggs1@gmail.com>
2020-03-26 09:16:38 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,13 @@
 d3.json("samples.json").then((data) => {
-var trace1 = {
-    x: data.id,
-    y: data.age,
-    type: "hbar",
-    name: "ID vs Age",
-    boxpoints: "all"
-};
+function getName(idNum) {
+    return idNum.names
+}
+console.log(data)
 
-var data = [trace1];
-
-var layout = {
-    title: "Some random title",
-    xaxis: {title: "ID"},
-    yaxis: {title: "Age"}
-};
-
-Plotly.newPlot("bar", data, layout);
-
-});
-
-d3.json("samples.json").then((data) => {
     var trace1 = {
-        x: data.id,
+        x: data.names,
         y: data.age,
-        type: "hbar",
+        type: "line",
         name: "ID vs Age",
         boxpoints: "all"
     };
@@ -32,23 +16,34 @@ d3.json("samples.json").then((data) => {
 
     var layout = {
         title: "Some random title",
-        xaxis: {
-            title: "ID"
-        },
-        yaxis: {
-            title: "Age"
-        }
+        xaxis: { title: "ID" },
+        yaxis: { title: "Age" }
+    };
+
+    Plotly.newPlot("bar", data, layout);
+
+    var trace1 = {
+        x: data.names,
+        y: data.age,
+        type: "line",
+        name: "ID vs Age",
+        boxpoints: "all"
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        title: "Some random title",
+        xaxis: { title: "ID" },
+        yaxis: { title: "Age" }
     };
 
     Plotly.newPlot("gauge", data, layout);
 
-});
-
-d3.json("samples.json").then((data) => {
     var trace1 = {
-        x: data.id,
+        x: data.names,
         y: data.age,
-        type: "hbar",
+        type: "line",
         name: "ID vs Age",
         boxpoints: "all"
     };
@@ -57,14 +52,99 @@ d3.json("samples.json").then((data) => {
 
     var layout = {
         title: "Some random title",
-        xaxis: {
-            title: "ID"
-        },
-        yaxis: {
-            title: "Age"
-        }
+        xaxis: { title: "ID" },
+        yaxis: { title: "Age" }
     };
 
     Plotly.newPlot("bubble", data, layout);
 
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // charts
+// d3.json("samples.json").then((data) => {
+// var trace1 = {
+//     x: data.id,
+//     y: data.age,
+//     type: "hbar",
+//     name: "ID vs Age",
+//     boxpoints: "all"
+
+
+// };
+
+// var data = [trace1];
+
+// var layout = {
+//     title: "Some random title",
+//     xaxis: {title: "ID"},
+//     yaxis: {title: "Age"}
+// };
+
+// Plotly.newPlot("bar", data, layout);
+
+// });
+
+// d3.json("samples.json").then((data) => {
+//     var trace1 = {
+//         x: data.id,
+//         y: data.age,
+//         type: "hbar",
+//         name: "ID vs Age",
+//         boxpoints: "all"
+//     };
+
+//     var data = [trace1];
+
+//     var layout = {
+//         title: "Some random title",
+//         xaxis: {
+//             title: "ID"
+//         },
+//         yaxis: {
+//             title: "Age"
+//         }
+//     };
+
+//     Plotly.newPlot("gauge", data, layout);
+
+// });
+
+// d3.json("samples.json").then((data) => {
+//     var trace1 = {
+//         x: data.id,
+//         y: data.age,
+//         type: "hbar",
+//         name: "ID vs Age",
+//         boxpoints: "all"
+//     };
+
+//     var data = [trace1];
+
+//     var layout = {
+//         title: "Some random title",
+//         xaxis: {
+//             title: "ID"
+//         },
+//         yaxis: {
+//             title: "Age"
+//         }
+//     };
+
+//     Plotly.newPlot("bubble", data, layout);
+
+// });
